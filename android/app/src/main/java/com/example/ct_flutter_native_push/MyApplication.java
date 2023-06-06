66614c824bd1581b2ac75e5fa5c11dc78c5f8bf3
Mashnu <mashnu@clevertap.com>
2023-06-06 15:28:53 +0530

    initial

--- android/app/src/main/java/com/example/ct_flutter_native_push/MyApplication.java
+++ android/app/src/main/java/com/example/ct_flutter_native_push/MyApplication.java
@@ -1,32 +1,16 @@
 package com.example.ct_flutter_native_push;
 
-import android.util.Log;
-
 import com.clevertap.android.sdk.ActivityLifecycleCallback;
 import com.clevertap.android.sdk.CleverTapAPI;
-import com.clevertap.android.sdk.pushnotification.CTPushNotificationListener;
-
-import java.util.HashMap;
 
 import io.flutter.app.FlutterApplication;
 
-public class MyApplication extends FlutterApplication implements CTPushNotificationListener{
+public class MyApplication extends FlutterApplication {
 
     @Override
     public void onCreate() {
         ActivityLifecycleCallback.register(this);
         CleverTapAPI.setDebugLevel(CleverTapAPI.LogLevel.DEBUG);
-        CleverTapAPI cleverTapAPI = CleverTapAPI.getDefaultInstance(getApplicationContext());
-        assert cleverTapAPI != null;
-        cleverTapAPI.setCTPushNotificationListener(this);
         super.onCreate();
-    }
-
-    @Override
-    public void onNotificationClickedPayloadReceived(HashMap<String, Object> payload) {
-        Log.d("Payload before: ", payload.toString());
-        if(payload != null){
-            Log.d("Payload clicked: ", payload.toString());
-        }
     }
 }
